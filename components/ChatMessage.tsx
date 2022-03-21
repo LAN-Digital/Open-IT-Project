18fd6acf7d6f3e6300fb4e017ff6e7df04d50649
Nemanja Grujic <luxisoop@gmail.com>
2022-03-20 23:10:35 -0600

    added send chat func

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -2,15 +2,18 @@ import { getAuth } from 'firebase/auth';
 import React from 'react'
 
 const ChatMessage = (props: any) => {
-    const { text, uid } = props.message;
+    const { text, uid, name } = props.message;
     const auth = getAuth();
     
     const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
     
   return (
-      <div className={`rounded max-w-xs my-2 bg-gray-500 px-4 py-2 ${messageClass == 'sent' ? 'ml-auto' : 'mr-auto'}`}>
+    <div className={`max-w-xs ${messageClass == 'sent' ? 'ml-auto' : 'mr-auto'}`}>
+      <h1 className='text-gray-200'>{name}</h1>
+      <div className={`rounded max-w-xs my-2 bg-gray-500 px-4 py-2 ${messageClass == 'sent' ? 'bg-green-600' : ''}`}>
     
     <p className='text-gray-200'>{text}</p>
+    </div>
     </div>
   )
 }
--- components/LiveChat.tsx
+++ components/LiveChat.tsx
@@ -1,21 +1,47 @@
-
-import { collection, limit, orderBy, query } from 'firebase/firestore'
-import React from 'react'
-import { useCollectionData, useCollection } from 'react-firebase-hooks/firestore';
-import { firestore } from './Firebase'
-import ChatMessage from './ChatMessage';
-
+import { collection, limit, orderBy, query, doc, setDoc, serverTimestamp } from "firebase/firestore";
+import React, { useRef } from "react";
+import {
+  useCollectionData,
+  useCollection,
+} from "react-firebase-hooks/firestore";
+import { firestore } from "./Firebase";
+import ChatMessage from "./ChatMessage";
+import { getAuth } from "firebase/auth";
 
 const LiveChat = () => {
-    const messagesRef = collection(firestore, 'messages');
-    const q = query(messagesRef, orderBy('createdAt'), limit(25));
-    const [messages, loading, error] = useCollectionData(q)
-    console.log(error);
-  return (
-    <div >
-        {messages && messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
-    </div>
-  )
-}
+  const chatRef = useRef();
+  const messagesRef = collection(firestore, "messages");
+  const q = query(messagesRef, orderBy("createdAt"), limit(25));
+  const [messages, loading, error] = useCollectionData(q);
+  const auth = getAuth();
 
-export default LiveChat
+  const sendMessage = async(e) => {
+    e.preventDefault();
+    const uid = auth.currentUser.uid;
+    const name = auth.currentUser.email
+
+    await setDoc(doc(messagesRef), {
+      text: chatRef.current.value,
+      createdAt: serverTimestamp(),
+      uid,
+      name
+    })
+  }
+
+  return (
+    <>
+      <div>
+        {messages &&
+          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+      </div>
+      <form onSubmit={sendMessage}>
+        <input type="text" id="chat" name="chat" ref={chatRef} required />
+        <button type="submit" className="bg-red-600 text-gray-200">
+          Send
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default LiveChat;
